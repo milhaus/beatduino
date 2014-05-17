be87ba60a78eaa1052d78141c8d21d65c79c9647
tack <tackyy@gmail.com>
2014-05-17 16:16:54 -0700

    decruft ardiono sketch

--- motion/motion_ard/motion_ard.ino
+++ motion/motion_ard/motion_ard.ino
@@ -10,13 +10,6 @@ const int xPin = 0;
 const int yPin = 1;
 const int zPin = 2;
 
-//The minimum and maximum values that came from
-//the accelerometer while standing still
-//You very well may need to change these
-int minVal = 265;
-int maxVal = 402;
-
-
 //to hold the caculated values
 double x;
 double y;
@@ -33,14 +26,6 @@ void loop(){
   int xRead = analogRead(xPin);
   int yRead = analogRead(yPin);
   int zRead = analogRead(zPin);
-
-  String outputt = "{\"x\":";
-  outputt += xRead;
-  outputt += ",\"y\":";
-  outputt += yRead;
-  outputt += ",\"z\":";
-  outputt += zRead;
-  outputt += "}";
   
   String tinyData = "";
   tinyData += xRead;
