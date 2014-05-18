b5b39b4c852c670c640a9326ab8c8a4cfdbba00a
Thorsten Sideb0ard <thorsten.sideb0ard@gmail.com>
2014-05-17 19:44:34 -0700

    gunzz

---
+++ gunshotz.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var amqp = require('./beatduino_lib');
+var exec = require('child_process').exec;
+
+var shot = "wavs/TrpDrumz/FX/FX_2_Gunshot.wav";
+
+function playrrr(){
+	exec("play " + shot);
+        //setTimeout( function() {
+        //  exec("say 'DANCE MOTHER FUCKER!'");
+        //}, 20);
+}
+
+amqp.subscribe('motion', function(msg) {
+  var x = msg.x, y = msg.y, z = msg.z, Gx = msg.Gx, Gy = msg.Gy, Gz = msg.Gz;
+  
+  if (Gz > -.5) {
+	  console.log("Dance human! : " + typeof Gz);
+	  playrrr();
+  }
+  
+  //console.log("Dance human! : " + Gz);
+});
